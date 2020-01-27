019c6ba3cd1a5bf31c2e39e8d354ffdd41f33533
Wang <zechenw@a483e7462a21.ant.amazon.com>
2020-01-27 16:31:50 +0800

    fix

--- src/gluonnlp/model/bert.py
+++ src/gluonnlp/model/bert.py
@@ -184,8 +184,8 @@ class DotProductSelfAttentionCell(HybridBlock):
         ones = F.ones_like(steps)
         mask = F.broadcast_lesser(F.reshape(steps, shape=(1, -1)),
                                   F.reshape(valid_length, shape=(-1, 1)))
-        mask = F.broadcast_mul(F.expand_dims(mask, axis=1),
-                               F.broadcast_mul(ones, F.reshape(ones, shape=(-1, 1))))
+        # mask = F.broadcast_mul(F.expand_dims(mask, axis=1),
+        #                        F.broadcast_mul(ones, F.reshape(ones, shape=(-1, 1))))
         return mask
 
     # pylint: disable=arguments-differ
@@ -207,7 +207,7 @@ class DotProductSelfAttentionCell(HybridBlock):
         #     att_weights = F.softmax(att_score, axis=-1)
         # att_weights shape = (batch_size, seq_length, seq_length)
         mask = self._padding_mask(F, att_score, valid_len)
-        print("mask shape: ", mask)
+        print("mask shape: ", mask.shape)
         att_weights = _masked_softmax(F, att_score, mask, np.float32)
         att_weights = self.dropout_layer(att_weights)
         context_vec = F.contrib.interleaved_matmul_selfatt_valatt(qkv_proj, att_weights,
